c95643adec5e9aed87ca675bc24c850ffb1b1790
chandraraeddy0910 <70686792+chandraraeddy0910@users.noreply.github.com>
2026-01-06 12:13:07 +0000

    Update chandra.sql

---
+++ chandra.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `auto_stats` (
+	`stat_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`c_ts` INT(10) UNSIGNED NOT NULL,
+	`name` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`description` TEXT NOT NULL COLLATE 'utf8_general_ci',
+	`function` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`status_id` INT(10) UNSIGNED NOT NULL,
+	`template_id` INT(10) UNSIGNED NOT NULL,
+	`active` INT(10) UNSIGNED NOT NULL DEFAULT '1',
+	`active_filters` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`stat_id`) USING BTREE,
+	UNIQUE INDEX `function` (`function`) USING BTREE,
+	INDEX `template_id` (`template_id`) USING BTREE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=3
+;
